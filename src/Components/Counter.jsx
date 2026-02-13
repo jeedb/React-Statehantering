5ecc6ab79fe67994bd0752abe882a30a3c443dc2
jeedb <jeedb7810@edu.sunne.se>
2026-02-13 14:40:45 +0100

    Added Counter

---
+++ src/Components/Counter.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+function Counter() {
+    const [count, setCount] = useState(0)
+
+    function increment() {
+        setCount(count + 1)
+    }
+
+    function decrement() {
+        setCount(count - 1)
+    }
+
+    function reset() {
+        setCount(0)
+    }
+
+    return (
+        <div>
+            <h2>Counter: {count}</h2>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    )
+}
+
+export default Counter
